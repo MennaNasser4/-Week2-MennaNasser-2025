ed4aeb050c643fc64fa1c55baf8055d4b2031b76
MennaNasser4 <mennabadr157@gmail.com>
2025-01-25 22:06:26 +0200

    Create sec4.cpp

---
+++ sec4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+   cout<<"Enter your favorite number between 1 and 100 : ";
+   cin>>n;
+   cout<<"Amazing!! That's my favorite number too!\n No really!!"<<n<<" is my favorite number!";
+
+    return 0;
+}
